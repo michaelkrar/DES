afead550ea06bae79df46dce543c93abdb41bc56
michaelkrar <michael.krar1@gmail.com>
2025-01-22 12:57:57 -0500

    Initial commit

---
+++ DES.java
@@ -0,0 +1,20 @@
+public class DES {
+    private int[] key;
+    private int keyLength;
+    public DES(){
+        this.key = new int[]{0,0,0,0,0,0,0,0,0,0};
+        keyLength = 10;
+    }
+    public DES(int[] key){
+        this.key=key;
+        this.keyLength=key.length;
+    }
+    public int[] encrypt(int[] plaintext){
+        
+        return null;
+    }
+    public int[] decrypt(int[] cyphertext){
+        return null;
+    }
+    
+}
